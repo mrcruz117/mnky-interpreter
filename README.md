5b64267664e095e064f0e162796551fea75390c2
Michael Cruz <mcruz@aersale.com>
2025-02-05 10:37:49 -0500

    tests passed

--- README.md
+++ README.md
@@ -6,8 +6,13 @@ An interpreter written in Go using only the std lib. Thank you Thorsten Ball!
 
 ### Parser
 
-- parse prefix operators
-- p.62
+- infix operators
+```bash
+  5 + 5;
+  5 == 5;
+  5 != 5;
+```
+- p.63
 
 <!-- ```bash
   lexer.go
--- parser/parser.go
+++ parser/parser.go
@@ -48,9 +48,12 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 
 	return p
 }
+
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
@@ -189,6 +192,18 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 
 	lit.Value = value
 	return lit
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+	p.nextToken()
+
+	expression.Right = p.parseExpression(PREFIX)
+
+	return expression
 }
 
 func (p *Parser) noPrefixParseFnError(t token.TokenType) {
