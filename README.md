f8580c65fc8e7308243a6e1e4114e9caff6e0893
Michael Cruz <mcruz@aersale.com>
2025-04-02 17:19:17 -0400

    removed todos passing all tests

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ An interpreter written in Go using only the std lib. Thank you Thorsten Ball!
 
 ### Parser
 
-- 2.8 Extending the parser
-- p.97 Removing Todos
-  - parser mostly complete
+- 2.9 Extending the parser
+- p.99 Read-parse-print-loop
+  - or read lex print loop
 #### TODO:
 - add tracing functions for parser deepdive
 
--- parser/parser.go
+++ parser/parser.go
@@ -300,9 +300,10 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// TODO: We're skipping the expressions until we
-	// encounter a semicolon
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -337,8 +338,8 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	// TODO: We're skipping the expressions until we
-	// encounter a semicolon
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
