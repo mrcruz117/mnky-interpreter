518ca61365a24522071bdd672d04fbd626d8575e
Michael Cruz <mcruz@aersale.com>
2025-04-11 11:43:16 -0400

    tests pass

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -33,6 +33,15 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 		return &object.ReturnValue{Value: val}
 
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{
+			Parameters: params,
+			Body:       body,
+			Env:        env,
+		}
+
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
 	case *ast.LetStatement:
