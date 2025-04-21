7e65955a45017efe9ee3cc4c9c92969d78579cb2
Michael Cruz <mcruz@aersale.com>
2025-04-21 15:22:01 -0400

    index expressions test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -735,3 +735,25 @@ func TestParsingArrayLiterals(t *testing.T) {
 	testInfixExpression(t, array.Elements[2], 3, "+", 3)
 
 }
+
+func TestParsingIndexExpressions(t *testing.T) {
+	input := "myArray[1 + 1];"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	stmt, _ := program.Statements[0].(*ast.ExpressionStatement)
+	indexExp, ok := stmt.Expression.(*ast.IndexExpression)
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.IndexExpression. got=%T", stmt.Expression)
+	}
+
+	if !testIdentifier(t, indexExp.Left, "myArray") {
+		return
+	}
+	if !testInfixExpression(t, indexExp.Index, 1, "+", 1) {
+		return
+	}
+}
