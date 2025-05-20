f50dbff3e6a2b37163729b0425a002525fb06610
Michael Cruz <mcruz@aersale.com>
2025-05-20 11:18:21 -0400

    if expression

--- ast/modify.go
+++ ast/modify.go
@@ -12,7 +12,6 @@ func Modify(node Node, modifier ModifierFunc) Node {
 
 	case *ExpressionStatement:
 		node.Expression = Modify(node.Expression, modifier).(Expression)
-
 	case *InfixExpression:
 		node.Left, _ = Modify(node.Left, modifier).(Expression)
 		node.Right, _ = Modify(node.Right, modifier).(Expression)
@@ -21,6 +20,16 @@ func Modify(node Node, modifier ModifierFunc) Node {
 	case *IndexExpression:
 		node.Left, _ = Modify(node.Left, modifier).(Expression)
 		node.Index, _ = Modify(node.Index, modifier).(Expression)
+	case *IfExpression:
+		node.Condition, _ = Modify(node.Condition, modifier).(Expression)
+		node.Consequence = Modify(node.Consequence, modifier).(*BlockStatement)
+		if node.Alternative != nil {
+			node.Alternative = Modify(node.Alternative, modifier).(*BlockStatement)
+		}
+	case *BlockStatement:
+		for i := range node.Statements {
+			node.Statements[i], _ = Modify(node.Statements[i], modifier).(Statement)
+		}
 	}
 
 	return modifier(node)
